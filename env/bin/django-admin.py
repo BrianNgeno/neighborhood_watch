1f81b1c75f06d04c98cdbb436048dd0869d8a21b
brianngeno <bkn.ngeno@gmail.com>
2018-10-20 00:03:11 +0300

    initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bknngeno/Documents/moringa_core/Django/neighborhood/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
