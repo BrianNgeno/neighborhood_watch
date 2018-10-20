51a995b44c130a83cd7e18c0c89f148ac64b7208
brianngeno <bkn.ngeno@gmail.com>
2018-10-20 17:28:19 +0300

    added receivers for new profiles

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+# class Profile(models.Model):
+#     Profile_photo = models.ImageField(upload_to = 'images/',blank=True)
+#     Bio = models.TextField(max_length = 50,null = True)
+#     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+#     rate = models.ManyToManyField('Project', related_name='image',max_length=30)
+
+#     def save_profile(self):
+#         self.save()
+
+#     @classmethod
+#     def get_by_id(cls, id):
+#         details = Profile.objects.get(user = id)
+#         return details
+
+#     @classmethod
+#     def filter_by_id(cls, id):
+#         details = Profile.objects.filter(user = id).first()
+#         return details
+    
+#     @classmethod
+#     def search_user(cls, name):
+#         userprof = Profile.objects.filter(user__username__icontains = name)
+#         return userprof
