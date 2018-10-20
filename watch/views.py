32341694c83a1a16cab699a2c27b9ffab7158493
brianngeno <bkn.ngeno@gmail.com>
2018-10-20 16:06:43 +0300

    added a date conversion view function

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse,Http404
+from django.contrib.auth.decorators import login_required
+from .models import Project,Profile
+from django.contrib.auth.models import User
+import datetime as dt
 
 # Create your views here.
+
+def convert_dates(dates):
+    # function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','thursday','Friday','Saturday','Sunday']
+    '''
+    Returns the actual day of the week
+    '''
+    day = days[day_number]
+    return day
