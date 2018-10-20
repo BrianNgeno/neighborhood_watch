f15edddfd38fbcb3b3997e512a1e221229541353
brianngeno <bkn.ngeno@gmail.com>
2018-10-20 16:11:59 +0300

    added the requirements txt and addded all requirements

--- watch/views.py
+++ watch/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse,Http404
 from django.contrib.auth.decorators import login_required
-from .models import Project,Profile
+# from .models import Project,Profile
 from django.contrib.auth.models import User
 import datetime as dt
 
