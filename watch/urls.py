aa474210c5f6ed6071ab75bb15e1482284f62411
brianngeno <bkn.ngeno@gmail.com>
2018-10-20 16:35:32 +0300

    added the image upload configs to app urls

--- watch/urls.py
+++ watch/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns=[
     url('^$',views.home_page,name = 'home_page'),
-    ]
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
