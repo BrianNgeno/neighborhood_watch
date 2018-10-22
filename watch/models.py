20a038df702cc68d84fe7d47934427ca6d9b4c0e
brianngeno <bkn.ngeno@gmail.com>
2018-10-22 22:50:20 +0300

    added a function for saving comment and ordering

--- watch/models.py
+++ watch/models.py
@@ -105,3 +105,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['-id']
+
+    def save_comment(self):
+        self.save()
+
+    
