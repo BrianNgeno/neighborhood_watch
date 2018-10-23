b404c19057b0ffac5cc738e83b935cd945ac6c43
brianngeno <bkn.ngeno@gmail.com>
2018-10-23 12:32:15 +0300

    added test for comments

--- watch/tests.py
+++ watch/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from .models import Comment, Business
 # Create your tests here.
+class CommentTestCase(TestCase):
+    '''
+    setup
+    '''
+    def setUp(self):
+        self.comment = Comment(name='lovely')
+    '''
+    test instance of comment
+    '''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment,Comment))
+        '''
+        test for save instance of comment
+        '''
+    def test_save_comment(self):
+        self.comment.save_comment()
+        name = Comment.objects.all()
+        self.assertTrue(len(name)>0)
